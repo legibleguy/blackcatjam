2273de429a38b1bf379b021d3d812f03f3e5c609
legibleguy <solovyovandy@yandex.ru>
2024-06-28 17:28:46 -0500

    avoidance stuff

--- scripts/player/blackhole.gd
+++ scripts/player/blackhole.gd
@@ -8,25 +8,12 @@ signal player_ready(in_player)
 
 func _ready():
 	player_ready.emit(self)
-	
-	if is_player:
-		var startup_anim = Animation.new()
-		var track_index = startup_anim.add_track(Animation.TYPE_VALUE)
-		startup_anim.track_set_path(track_index, "BlackHoleSprite:position:x")
-		startup_anim.track_insert_key(track_index, 0.0, 0.0)
-		startup_anim.track_insert_key(track_index, 2.0, 255.0)
-		startup_anim.length = 2.0
-		startup_anim.value_track_set_update_mode(track_index, Animation.UPDATE_CAPTURE)
-		
-		var animplayer = AnimationPlayer.new()
-		var lib = AnimationLibrary.new()
-		lib.add_animation("startup", startup_anim)
-		
-		animplayer.add_animation_library("lib", lib)
-		animplayer.play("startup")
-		
 
 func _physics_process(delta: float) -> void:
+	if $black_hole_rotation != null:
+		$black_hole_rotation.rotation += 0.1
+	else:
+		printerr("blackhole.gd: couldn't access $black_hole_rotation node")
 
 	if Input.is_action_pressed("move_l") and not Input.is_action_pressed("move_r"):
 		velocity.x = -SPEED
@@ -39,7 +26,3 @@ func _physics_process(delta: float) -> void:
 		velocity.y = SPEED
 
 	move_and_slide()
-
-
-func _on_child_entered_tree(node: Node) -> void:
-	pass # Replace with function body.
